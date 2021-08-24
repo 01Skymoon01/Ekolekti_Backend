6c9f55ae990e2bfba1f0d6ed27da3885ee42f233
01Skymoon01 <48031994+01Skymoon01@users.noreply.github.com>
2021-08-24 12:12:10 +0100

    update user register

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,7 +32,7 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body
+    const { name, email, password, gender, dateBirth } = req.body
 
     const userExists = await User.findOne({ email })
 
@@ -48,7 +48,9 @@ const registerUser = asyncHandler(async (req, res) => {
     const user = await User.create({
         name,
         email,
-        passwordCrypt
+        passwordCrypt,
+        gender,
+        dateBirth
     })
 
     if (user) {
