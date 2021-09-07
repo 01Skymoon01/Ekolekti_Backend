145ce5d774b07a00ad812c11045be867c0fff8eb
01Skymoon01 <48031994+01Skymoon01@users.noreply.github.com>
2021-09-07 21:34:39 +0100

    some desc for exchange model

--- models/exchangeModel.js
+++ models/exchangeModel.js
@@ -2,15 +2,21 @@ import mongoose from 'mongoose'
 
 const exchangeSchema = mongoose.Schema(
     {
+        // Wa9eth saret exchange
         date_exchange: {
             type: Date,
             required: false,
         },
+        // Position Client
         position: {
             type: String,
             required: false,
             default: ""
         },
+        // Etapes:
+        // 0 (notification)=> type: connu + quantity: estimation + score: estimation (bch n3awnou client yaaref score) + status: 0
+        // 1 (durant l'exchange)=> type: connu(checkout) + quantity: mesure + score: calculer + status: 0
+        // 2 (validation de l'exhange)=> type: connu(checkout) + quantity: validé + score: calculer + status: 1
         quantities: [ {
             type: {
                 type: String,
